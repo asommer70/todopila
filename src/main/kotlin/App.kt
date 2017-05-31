2c042bed6812993de5be3b7dbc60eb908170193c
Adam Sommer <adam@thehoick.com>
2017-05-31 01:12:39 -0400

    Have version and help working from the command line.

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,9 +1,44 @@
 
 package todopila
+val version = "0.0.1"
 
-fun main(args: Array<String>) {
-	println()
+fun main(args: Array<String>) {	
+	//
+	// Handle command line arguments.
+	//
+	if (args.size != 0) {
+		when (args[0]) {
+			"--help" -> printHelp()
+			"-h" -> printHelp()
+			"--version" -> printVersion()
+			"-v" -> printVersion()
+			else -> {
+				// TODO:as set active List as arg[1]...
+				print("List set to:  " + args[0])
+		    }
+		}	
+	}
+			
+	println("Welcome to ToDo Pila!")
+	println("press h for a list of commands.")
+	
+	// TODO:as Display a list of Lists.
+	
 	println("Hello World")
-	println("Beans...\tWhat...")
+//	var input = readLine()
+//	println("You said: " + input)
 	println()
+}
+
+fun printHelp() {
+	println("Usage: todopila [options] [list_name]")
+	println("where options include:")
+	println("\t--version, -v\t\tprint product version and exit")
+	println("\t--help, -h\t\tprint this help message and exit")
+	System.exit(0)
+}
+
+fun printVersion() {
+	println("Todo Pila! version: " + version)
+	System.exit(0)
 }
